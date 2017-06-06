da45f42bfafc60ffceb4ef7d96caf23f181b929d
AbdulSammad786 <hasae300@gmail.com>
2017-06-06 23:45:31 +0500

    First Referesh

--- app/src/main/java/com/example/abdulsamad/bible/Adapters/BooksAdapter.java
+++ app/src/main/java/com/example/abdulsamad/bible/Adapters/BooksAdapter.java
@@ -1,18 +1,13 @@
 package com.example.abdulsamad.bible.Adapters;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-
 import com.example.abdulsamad.bible.DataProviders.BooksDataProvider;
 import com.example.abdulsamad.bible.R;
-
-
 import java.util.ArrayList;
-
 /**
  * Created by DELL on 24-08-2016.
  */
@@ -25,22 +20,18 @@ public class BooksAdapter extends BaseAdapter {
         this.serviceManualArrayList = serviceManualArrayList;
         inflater = LayoutInflater.from(context);
     }
-
     @Override
     public int getCount() {
         return serviceManualArrayList.size();
     }
-
     @Override
     public Object getItem(int i) {
         return i;
     }
-
     @Override
     public long getItemId(int i) {
         return i;
     }
-
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         Holder holder;
